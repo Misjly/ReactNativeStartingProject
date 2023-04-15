ca13fc20786cb2c5544555cda1e03f33e8dfa5d2
Misjly <nikmisg@gmail.com>
2023-04-15 11:34:46 -0500

    Week 4 Assignment

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -239,19 +239,20 @@ const Main = () => {
         dispatch(fetchComments());
     }, [dispatch]);
 
-    useEffect(() => {
-        NetInfo.fetch().then((connectionInfo) => {
-            Platform.OS === 'ios'
-                ? Alert.alert(
-                      'Initial Network Connectivity Type:',
-                      connectionInfo.type
-                  )
-                : ToastAndroid.show(
-                      'Initial Network Connectivity Type: ' +
-                          connectionInfo.type,
-                      ToastAndroid.LONG
-                  );
-        });
+    const showNetInfo = async () => {
+        const connectionInfo = await NetInfo.fetch();
+        
+        Platform.OS === 'ios'
+            ? Alert.alert(
+                    'Initial Network Connectivity Type:',
+                    connectionInfo.type
+                )
+            : ToastAndroid.show(
+                    'Initial Network Connectivity Type: ' +
+                        connectionInfo.type,
+                    ToastAndroid.LONG
+                );
+        
 
         const unsubscribeNetInfo = NetInfo.addEventListener(
             (connectionInfo) => {
@@ -260,6 +261,10 @@ const Main = () => {
         );
 
         return unsubscribeNetInfo;
+    }
+
+    useEffect(() => {
+        showNetInfo();
     }, []);
 
     const handleConnectivityChange = (connectionInfo) => {
